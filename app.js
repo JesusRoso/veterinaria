29bacf4f22526244fb8a4844f7d22849935d0de6
Jesus Roso <jr8campeones@gmail.com>
2022-05-26 06:39:26 -0500

    Creacion variables de entorno

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+//variables de entorno
+require('dotenv').config();
 const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
@@ -9,9 +11,9 @@ app.use(express.static(__dirname+"/public"))
 
 //conexión a mongo por medio de mongoose
 const mongoose = require('mongoose');
-const usuario = "admin_mascotas"
-const password = "8Xh2pfWyglSulkst"
-const dbName = "veterinaria"
+const usuario = process.env.USUARIO;
+const password = process.env.PASSWORD
+const dbName = process.env.DBNAME;
 const uri = `mongodb+srv://${usuario}:${password}@cluster0.dqo3v.mongodb.net/${dbName}?retryWrites=true&w=majority`
 mongoose.connect(uri, {useNewUrlParser:true, useUnifiedTopology:true})
 .then(()=>console.log('conectado a mongodb'))
@@ -21,10 +23,6 @@ mongoose.connect(uri, {useNewUrlParser:true, useUnifiedTopology:true})
 
 app.use('/',require('./router/RutasWeb'));
 app.use('/mascotas',require('./router/Mascotas'));
-// app.use((req, res, next)=>{
-//   // res.status(404).sendFile(__dirname+"/public/404.html");
-//   res.status(404).render("404",{titulo: "Pagina 404"});
-// })
 
 
 app.use((req, res, next)=>{
@@ -34,5 +32,5 @@ app.use((req, res, next)=>{
   })
 })
 app.listen(port, ()=>{
-    console.log("Escuchando express");
+    console.log("Escuchando express "+port);
 })
--- router/Mascotas.js
+++ router/Mascotas.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res)=>{
-    res.render("mascotas",{listaMascotas:"Aquí irán todas las mascotas"})
+const mascota = require('../models/mascotas');
+
+router.get('/', async (req, res)=>{
+    try
+    {
+        const arrayMascotas = await mascota.find();
+        console.log(arrayMascotas);
+        res.render("mascotas",{listaMascotas:"Aquí irán todas las mascotas",arrayMascotas})
+    }
+    catch(error)
+    {
+        console.log(error)
+    }
+    
 })
 
 module.exports= router;
